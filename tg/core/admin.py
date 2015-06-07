32a8a569fdc4b5adc2f84b144c3c513475c6fb4a
Odin Hørthe Omdal <odin.omdal@gmail.com>
2015-06-07 19:09:28 +0200

    Add django-reversions to see changes done in the admin

--- tg/core/admin.py
+++ tg/core/admin.py
@@ -1,10 +1,11 @@
+import reversion
 from django.contrib import admin
 
 from core.models import Entry
 
 
 @admin.register(Entry)
-class EntryAdmin(admin.ModelAdmin):
+class EntryAdmin(reversion.VersionAdmin):
     search_fields = ('full_name', 'email', 'address',)
     list_display = ('__str__', 'status', 'sort_name',
                     'address', 'email', 'postnummer')
