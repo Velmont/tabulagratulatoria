5333eb449cbb12883a24e59c5e5948e7bd7feb4b
Odin Hørthe Omdal <odin.omdal@gmail.com>
2015-06-07 18:49:12 +0200

    Add search field for entries in the django admin

--- tg/core/admin.py
+++ tg/core/admin.py
@@ -5,5 +5,6 @@ from core.models import Entry
 
 @admin.register(Entry)
 class EntryAdmin(admin.ModelAdmin):
+    search_fields = ('full_name', 'email', 'address',)
     list_display = ('__str__', 'status', 'sort_name',
                     'address', 'email', 'postnummer')
