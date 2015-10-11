9f01d5dfdd5b80b80c9be0d1871efa2673fab974
Odin Hørthe Omdal <odin.omdal@gmail.com>
2015-10-11 16:49:39 +0200

    Place database where gunicorn can write to it in production

--- tg/tg/settings/production.py
+++ tg/tg/settings/production.py
@@ -6,3 +6,10 @@ from .base import *
 SECRET_KEY = get_env_setting('SECRET_KEY')
 
 STATIC_ROOT = os.path.join(REPO_DIR, 'collected_staticfiles')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(REPO_DIR, 'db', 'db.sqlite3'),
+    }
+}
