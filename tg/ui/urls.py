c2c439b3709efade0c634860030b86b77adeeb35
Odin Hørthe Omdal <odin.omdal@gmail.com>
2017-07-12 21:44:00 +0200

    Skjema på framsida

--- tg/ui/urls.py
+++ tg/ui/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^add/$', views.adder, name='adder'),
+    url(r'^$', views.adder, name='adder'),
+    url(r'^home/$', views.home, name='home'),
+    url(r'^add/$', views.adder),
     url(r'^takk/$', views.takk, name='takk'),
 ]
